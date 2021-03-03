c896d15a692d4da1491967671fda8b1b7d49d7f2
ng.h.khoi <ng.h.khoi@outlook.com>
2021-03-03 12:18:25 +0700

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
 // Mock service worker
+self.addEventListener('install', (event) => {
+  console.log('👷', 'install', event);
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('👷', 'activate', event);
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  console.log('👷', 'fetch', event);
 });
